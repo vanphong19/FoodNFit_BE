e381107a84446f6e0e248a953f919da54bfc4859
vanphong19 <phongdv1901@gmail.com>
2025-06-14 16:37:38 +0700

    step tracking

--- src/main/java/com/vanphong/foodnfitbe/presentation/viewmodel/request/StepsTrackingRequest.java
+++ src/main/java/com/vanphong/foodnfitbe/presentation/viewmodel/request/StepsTrackingRequest.java
@@ -1,5 +1,6 @@
 package com.vanphong.foodnfitbe.presentation.viewmodel.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -12,7 +13,9 @@ import java.time.LocalDateTime;
 @Setter
 public class StepsTrackingRequest {
     private Integer stepsCount;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")  // <-- Thêm dòng này
     private LocalDateTime startTime;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")  // <-- Thêm dòng này
     private LocalDateTime endTime;
     private Boolean isWalkingSession;
 }
